f3b702af894f37d70f4e0327c426ad211c180ea9
hyeonmi <kim.hyeonmi.0711@gmail.com>
2026-06-15 18:23:56 +0900

    refact: shadcn 버튼으로 교체

--- components/user/mission/MissionDetailModal.tsx
+++ components/user/mission/MissionDetailModal.tsx
@@ -7,7 +7,7 @@ import {
   DialogTitle,
   DialogDescription,
 } from "@/components/ui/dialog";
-import Button from "@/components/sample/Button";
+import { Button } from "@/components/ui/button";
 import { useModalHistoryBack } from "@/hooks/useModalHistoryBack";
 
 type Mission = {
