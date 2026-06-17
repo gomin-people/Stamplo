7d8ba4174cafc2c1fdf158a3369cee663ec35fa4
skylarkim22 <kim.hyeonmi.0711@gmail.com>
2026-06-17 15:18:33 +0900

    refact: next local font 로 변경 (#288)
    - preload 기능 제공
    - swap 옵션으로 대체 폰트 로딩

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,71 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 import Providers from "./providers";
+
+const notoSansKR = localFont({
+  src: [
+    {
+      path: "../public/fonts/noto-sans-kr-korean-400-normal.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/noto-sans-kr-latin-400-normal.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/noto-sans-kr-korean-700-normal.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/noto-sans-kr-latin-700-normal.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/noto-sans-kr-korean-800-normal.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/noto-sans-kr-latin-800-normal.woff2",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-noto-sans-kr",
+});
+
+const nanumGothic = localFont({
+  src: [
+    {
+      path: "../public/fonts/nanum-gothic-korean-700-normal.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/nanum-gothic-latin-700-normal.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/nanum-gothic-korean-800-normal.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/nanum-gothic-latin-800-normal.woff2",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-nanum-gothic",
+});
 
 export const metadata: Metadata = {
   title: "Stamplo",
@@ -13,7 +78,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className="h-full antialiased font-sans">
+    <html
+      lang="ko"
+      className={`h-full antialiased ${notoSansKR.variable} ${nanumGothic.variable}`}
+    >
       <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
       </body>
